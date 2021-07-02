03d9be20b0388919ef0dbcfc25e59ad360a5539a
ecleaver <erincleaver@grcc.email.edu>
2021-07-02 07:08:50 -0400

    update

--- script.js
+++ script.js
@@ -1,26 +1,46 @@
 console.log('hello!');
 
 class CoffeMachine {
-  constructor(coffe = coffe) {
-    let coffe = coffe;
+  constructor(coffe = "coffe") {
+    this.coffe = coffe;
+    this.process = document.getElementById('process');
   }
   // whater Heater()
-  waterHeater() {}
+  waterHeater() {
+    const heatWater = new Promise((response, reject) => {
+      setTimeout(()=> { response('Heating water')}, 1000)
+    });
+    heatWater.then((response) => {
+      this.process.innerHTML = response
+    }, (error) => {
+      this.process.innerHTML ="Failed to create coffe";
+    })
+
+  }
   // GrindCoffe
-  grindCoffe() {}
+  grindCoffe() {
+    const grindCoffee = new Promise((response, reject) => {
+      setTimeout(()=> { response('Grinding Coffe')}, 3000)
+    });
+    grindCoffee.then((response) => {
+      this.process.innerHTML = response
+    }, (error) => {
+      this.process.innerHTML ="Failed to create coffe"
+    })
+  }
   brewCoffee() {
     this.waterHeater();
     this.grindCoffe();
     setTimeout(() => {
-      console.log(`Your ${this.coffe} is READY!`);
+      this.process.innerHTML =`Your ${this.coffe} Coffe is READY!`;
     }, 6000);
   }
 }
 
-function brewCoffeeClick() {
-  const coffeMaker = new CoffeMachine();
-  console.log(coffeMaker);
+function brewCoffeeClick(coffe) {
+  
+  const coffeMaker = new CoffeMachine(coffe.id);
+  //console.log(coffeMaker);
   coffeMaker.brewCoffee();
 }
 
-window.test = test;
